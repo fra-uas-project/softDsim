7101cc644443db34b7a6dece935c3590ca0f9b63
Edg3 <64014742+pfrechFra@users.noreply.github.com>
2022-06-01 21:32:07 +0200

    bugfixes before push

--- frontend/src/components/MotivationPanel.jsx
+++ frontend/src/components/MotivationPanel.jsx
@@ -15,29 +15,29 @@ const MotivationPanel = () => {
             <Text size='lg' fontWeight='bold' mb='2' color='black'>
                 <Heading size='md' fontWeight='bold'  p="5">Motivation Overview<hr></hr></Heading>
                 <SimpleGrid minChildWidth='120px' spacing='40px'>
-  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md'>Senior Developer
-            <CircularProgress value={40} color='blue.400' size='65'>
-            <CircularProgressLabel>40%</CircularProgressLabel>
+  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md' p='1'>Senior Developer
+            <CircularProgress value={80} color='blue.400' size='65'>
+            <CircularProgressLabel>80%</CircularProgressLabel>
             </CircularProgress></Box>
-  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md'> Senior Frontend
+  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md' p='1'> Senior Frontend
             <CircularProgress value={30} color='blue.400' size='65'>
             <CircularProgressLabel>30%</CircularProgressLabel>
             </CircularProgress></Box>
-  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md'>Junior Developer
-            <CircularProgress value={75} color='blue.400' size='65'>
-            <CircularProgressLabel>75%</CircularProgressLabel>
+  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md' p='1'>Junior Developer
+            <CircularProgress value={90} color='blue.400' size='65'>
+            <CircularProgressLabel>90%</CircularProgressLabel>
             </CircularProgress></Box>
-  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md'>Some Developer
+  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md' p='1'>Some Developer
             <CircularProgress value={60} color='blue.400' size='65'>
             <CircularProgressLabel>60%</CircularProgressLabel>
             </CircularProgress></Box>
-  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md'>Backend Developer
-            <CircularProgress value={15} color='blue.400' size='65'>
-            <CircularProgressLabel>15%</CircularProgressLabel>
+  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md' p='1'>Backend Developer
+            <CircularProgress value={45} color='blue.400' size='65'>
+            <CircularProgressLabel>45%</CircularProgressLabel>
             </CircularProgress></Box>
-  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md'>Backend Developer
-            <CircularProgress value={15} color='blue.400' size='65'>
-            <CircularProgressLabel>15%</CircularProgressLabel>
+  <Box bg='blue.100' height='100%' borderRadius='2xl' boxShadow='md' p='1'>Backend Developer
+            <CircularProgress value={65} color='blue.400' size='65'>
+            <CircularProgressLabel>65%</CircularProgressLabel>
             </CircularProgress></Box>
 </SimpleGrid>
             </Text>
