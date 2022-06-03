f23f527a4b3deb85b6653cf3b89a67ae4b9e3bde
Leonard Husske <leonard.husske@t-online.de>
2022-06-03 10:55:36 +0200

    refactor: #290 Replace direct access to an object with immutable object of event inspector form
    This is the react way to change objects

--- frontend/src/pages/ScenarioStudio.jsx
+++ frontend/src/pages/ScenarioStudio.jsx
@@ -473,6 +473,7 @@ const ScenarioStudio = () => {
                                                     <EventInspectorForm
                                                         key={selectedObject.id}
                                                         eventData={findComponent(selectedItem)}
+                                                        updateEditorList={updateEditorList}
                                                     />
                                                 }
 
