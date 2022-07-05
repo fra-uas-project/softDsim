c4011e4b3f5a8deeedd8088d6f7dfa5b9ea330a2
DontEatMyCookies <dustinnoack@gmx.de>
2022-07-05 13:11:02 +0200

    task: #503 - adding new input fields and api call

--- frontend/src/pages/AddMultipleUsers.jsx
+++ frontend/src/pages/AddMultipleUsers.jsx
@@ -1,4 +1,4 @@
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Container, Flex, Grid, Heading, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from "@chakra-ui/react"
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Container, Flex, Grid, Heading, Input, Text, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from "@chakra-ui/react"
 import { useState } from "react"
 import { HiChevronRight } from "react-icons/hi"
 
@@ -11,11 +11,20 @@ const AddMultipleUsers = () => {
     // user count state
     const [userCount, setUserCount] = useState(1)
 
+    // password length
+    const [passwordLength, setPasswordLength] = useState(5)
+
+    // starting index
+    const [startingIndex, setStartingIndex] = useState(0)
+
     // button state
     const [usersGenerated, setUsersGenerated] = useState(false)
 
     // generated user csv
     const [userCsv, setUserCsv] = useState('')
+
+    // error state
+    const [errorState, setErrorState] = useState('none')
 
     // save entered prefix
     function prefixInput(input) {
@@ -24,14 +33,53 @@ const AddMultipleUsers = () => {
 
     // save entered user count
     function handleCountChange(input) {
-        setUserCount(input)
+        setUserCount(parseInt(input))
+    }
+
+    // save password length
+    function handlePasswordLength(input) {
+        setPasswordLength(parseInt(input))
+    }
+
+    // handle starting index input
+    function handleIndexInput(input) {
+        setStartingIndex(parseInt(input))
     }
 
     // create users in backend
-    function createUsers() {
-        console.log(prefix, userCount)
-        setUserCsv('ID; Password;\n 123; Test1234;\n 123; Test1234;\n 123; Test1234;\n 123; Test1234')
-        setUsersGenerated(true)
+    async function createUsers() {
+        try {
+            // create users api call
+            const res = await fetch(`${process.env.REACT_APP_DJANGO_HOST}/api/user/create-many`, {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify({ "prefix": prefix, "count": userCount, "pw-length": passwordLength, "start-index": startingIndex }),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+
+            // get user data
+            const userData = await res.json()
+
+            if (res.status === 201) {
+                // table head
+                var tempUsers = 'ID; Password;\r'
+                // generate userlist
+                for (const user of userData.data) {
+                    tempUsers = tempUsers + `${user.username};${user.password};\r`
+                }
+                setUserCsv(tempUsers)
+                setUsersGenerated(true)
+                setErrorState('success')
+            } else {
+                setErrorState('error')
+                console.log('nope')
+            }
+        } catch (error) {
+            console.log(error)
+            setErrorState('error')
+        }
     }
 
     return (
@@ -51,23 +99,70 @@ const AddMultipleUsers = () => {
                     <Box backgroundColor="white" borderRadius="2xl" w="full">
                         <Container maxW='6xl' pt={10} h="full" w="full" pb={10} minH="70vh" maxH="70vh" >
                             <Grid templateColumns='repeat(2, 1fr)' gap={5}>
-                                {/* prefix input */}
-                                <Input type="text" placeholder="Prefix (e.g. SoSe22)" size='lg' bg='#efefef' onChange={prefixInput} />
-                                {/* count input */}
-                                <NumberInput min={1} defaultValue={1} size='lg' onChange={handleCountChange}>
-                                    <NumberInputField />
-                                    <NumberInputStepper>
-                                        <NumberIncrementStepper />
-                                        <NumberDecrementStepper />
-                                    </NumberInputStepper>
-                                </NumberInput>
+                                <Grid>
+                                    {/* prefix input */}
+                                    <Text fontWeight="bold">Prefix</Text>
+                                    <Input type="text" placeholder="Prefix (e.g. SoSe22)" size='lg' bg='#efefef' onChange={prefixInput} />
+                                </Grid>
+                                <Grid>
+                                    {/* count input */}
+                                    <Text fontWeight="bold">User count</Text>
+                                    <NumberInput min={1} defaultValue={1} size='lg' onChange={handleCountChange}>
+                                        <NumberInputField />
+                                        <NumberInputStepper>
+                                            <NumberIncrementStepper />
+                                            <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </Grid>
+                                <Grid>
+                                    {/* password length */}
+                                    <Text fontWeight="bold">Password Length</Text>
+                                    <NumberInput min={5} defaultValue={5} size='lg' onChange={handlePasswordLength}>
+                                        <NumberInputField />
+                                        <NumberInputStepper>
+                                            <NumberIncrementStepper />
+                                            <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </Grid>
+                                <Grid>
+                                    {/* starting index */}
+                                    <Text fontWeight="bold">Starting Index</Text>
+                                    <NumberInput min={0} defaultValue={0} size='lg' onChange={handleIndexInput}>
+                                        <NumberInputField />
+                                        <NumberInputStepper>
+                                            <NumberIncrementStepper />
+                                            <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </Grid>
                             </Grid>
+                            {/* error or success messages */}
+                            <Flex align="center" justify="center" my={5}>
+                                {
+                                    errorState === 'success' ?
+                                        <>
+                                            <Text color="green" fontSize='xl' fontWeight="bold">
+                                                Users created successfully
+                                            </Text>
+                                        </> :
+                                        errorState === 'error' ?
+                                            <>
+                                                <Text color="red" fontSize='xl' fontWeight="bold">
+                                                    Users were not created successfully
+                                                </Text>
+                                            </> :
+                                            <></>
+                                }
+                                <Text></Text>
+                            </Flex>
                             {/* buttons */}
                             <Flex align="center" justify="center" my={5}>
                                 {usersGenerated ?
                                     <>
-                                        <a href={`data:text/csv;charset=utf-8,${userCsv}`} download="userList.csv">
-                                            <Button colorScheme="blue">
+                                        <a href={`data:text/csv;charset=utf-8,${encodeURIComponent(userCsv)}`} download="userList.csv">
+                                            <Button colorScheme="blue" size="lg">
                                                 Download
                                             </Button>
                                         </a>
