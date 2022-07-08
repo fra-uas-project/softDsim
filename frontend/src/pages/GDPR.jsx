2448b3a5b87353793042f4891d3448276da818ff
LucasRue <lucas.rue@gmx.de>
2022-07-08 15:15:26 +0200

    task: #513 Final Changes to Legal Pages

--- frontend/src/pages/GDPR.jsx
+++ frontend/src/pages/GDPR.jsx
@@ -12,8 +12,8 @@ const GDPR = () => {
                 p='5'
             >
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <p>
-                        Data protection is of a particularly high priority for the management of Simplify. The use of the Simplify is only possible with a valid FRA-UAS mail address. However, if the subject wants to create an account without the FRA-UAS mail domain, a staff member can create an account without using a mail address.
+                    <p >
+                        Data protection is of a particularly high priority for the management of Simplify. The use of Simplify is only possible with a valid Simplify Account. In order to get an account the responsible lecturer will create one.
                         The processing of personal data, such as the e-mail address of a data subject shall always be in line with the General Data Protection Regulation (GDPR), and in accordance with the country-specific data protection regulations applicable to Simplify. By means of this data protection declaration, Simplify would like to inform the general public of the nature, scope, and purpose of the personal data we collect, use and process. Furthermore, data subjects are informed, by means of this data protection declaration, of the rights to which they are entitled.
                         As the controller, Simplify has implemented numerous technical and organizational measures to ensure the most complete protection of personal data processed through this website. However, Internet-based data transmissions may in principle have security gaps, so absolute protection may not be guaranteed.
 
@@ -25,60 +25,62 @@ const GDPR = () => {
 
                     </p>
 
-                    <p>Simplyfy</p>
+                    <p>Simplify</p>
                     <p>Nibelungenplatz 1</p>
                     <p>60318 Frankfurt</p>
                     <p>Germany</p>
-                    <p>Phone: +49 69 1533-0</p>
-                    <p>Email: webredaktion@fra-uas.de</p>
+                    <p>Phone: +49 69 1533-2727 </p>
+                    <p>Email: hefter@fb2.fra-uas.de</p>
                     <p>Website: https://www.frankfurt-university.de/</p>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
                     <h4><b>2. Name and Address of the Data Protection Officer</b></h4>
                     <p>The Data Protection Officer of the controller is:</p>
-                    Max Mustermann
-                    <p>Simplyfy</p>
+                    <p>Simplify</p>
                     <p>Nibelungenplatz 1</p>
                     <p>60318 Frankfurt</p>
                     <p>Germany</p>
-                    <p>Phone: +49 69 1533-0</p>
-                    <p>Email: dsb@fra-uas.de</p>
+                    <p>Phone: +49 69 1533-2727 </p>
+                    <p>Email: hefter@fb2.fra-uas.dee</p>
                     <p>Website: https://www.frankfurt-university.de/</p>
                     <p>Any data subject may, at any time, contact our Data Protection Officer directly with all questions and suggestions concerning data protection.</p>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <h4><b>3. Cookies</b></h4>
+                    <h4><b>3. Legal basis for the processing </b></h4>
+                    <p>Art. 6(1) lit. a GDPR serves as the legal basis for processing operations for which we obtain consent for a specific processing purpose. Consent of the data subject is required before every login. Therefore the data subject needs to check a box in order to express the confirmation.</p>
+                </GridItem>
+                <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
+                    <h4><b>4. Cookies</b></h4>
                     <p>
                         The Internet pages of Simplify use cookies. Cookies are text files that are stored in a computer system via an Internet browser. Our application stores session cookies for each user. These store necessary information to identify a specific session. Through the use of cookies, Simplify can provide the applications core functionalities.
                         The data subject may, at any time, prevent the setting of cookies through our website by means of a corresponding setting of the Internet browser used, and may thus permanently deny the setting of cookies. Furthermore, already set cookies may be deleted at any time via an Internet browser or other software programs. This is possible in all popular Internet browsers. If the data subject deactivates the setting of cookies in the Internet browser used, not all functions of our website may be entirely usable.
                     </p>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <h4><b>4. Collection of general data and information</b></h4>
+                    <h4><b>5. Collection of general data and information</b></h4>
                     <p>
-                        The website of Simplify collects a series of general data and information when a data subject or automated system calls up the website. This general data and information are stored in the server log files. Collected may be (1) the browser types and versions used, (2) the operating system used by the accessing system, (3) the website from which an accessing system reaches our website (so-called referrers), (4) the sub-websites, (5) the date and time of access to the Internet site, (6) an Internet protocol address (IP address), (7) the Internet service provider of the accessing system, and (8) any other similar data and information that may be used in the event of attacks on our information technology systems.
-                        When using these general data and information, Simplify does not draw any conclusions about the data subject. Rather, this information is needed to (1) deliver the content of our website correctly, (2) optimize the content of our website as well as its advertisement, (3) ensure the long-term viability of our information technology systems and website technology, and (4) provide law enforcement authorities with the information necessary for criminal prosecution in case of a cyber-attack. Therefore, Simplify analyzes anonymously collected data and information statistically, with the aim of increasing the data protection and data security of our enterprise, and to ensure an optimal level of protection for the personal data we process. The anonymous data of the server log files are stored separately from all personal data provided by a data subject.
+                        The website of Simplify collects a series of general data and information when a data subject or automated system calls up the website. This general data and information are stored in the server log files. Collected may be authentication data required for login, email addresses, input data that every user makes when using the simulation
+                        When using Simplify these general data and information may be used to distribute extra points during  the grading process.
                     </p>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <h4><b>5. Registration on our website</b></h4>
+                    <h4><b>6. Registration on our website</b></h4>
                     <p>
-                        The data subject has the possibility to register on the website of the controller with the indication of personal data. Which personal data are transmitted to the controller is determined by the respective input mask used for the registration. The personal data entered by the data subject are collected and stored exclusively for internal use by the controller, and for his own purposes.
-                        By registering on the website of the controller, the IP address—assigned by the Internet service provider (ISP) and used by the data subject—date, and time of the registration are also stored.
-                        The registration of the data subject, with the voluntary indication of personal data, is intended to enable the controller to offer the data subject contents or services that may only be offered to registered users due to the nature of the matter in question. Registered persons are free to change the personal data specified during the registration at any time, or to have them completely deleted from the data stock of the controller.
+                        Only the admin or controller can register users on the website. This is mainly necessary to register students that want to achieve extra points during the grading process. The personal data entered by the controller is collected and stored exclusively for internal use by the controller, and for his own purposes.
+                        By logging in, the user accepts the processing of their personal data such as listed under chapter 5.
                         The data controller shall, at any time, provide information upon request to each data subject as to what personal data are stored about the data subject. In addition, the data controller shall correct or erase personal data at the request or indication of the data subject, insofar as there are no statutory storage obligations. The entirety of the controller’s employees are available to the data subject in this respect as contact persons.
 
                     </p>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <h4><b>6. Routine erasure and blocking of personal data</b></h4>
+                    <h4><b>7. Routine erasure and blocking of personal data</b></h4>
                     <p>
                         The data controller shall process and store the personal data of the data subject only for the period necessary to achieve the purpose of storage, or as far as this is granted by the European legislator or other legislators in laws or regulations to which the controller is subject to.
                         If the storage purpose is not applicable, or if a storage period prescribed by the European legislator or another competent legislator expires, the personal data are routinely stored in historical databases in accordance with legal requirements.
                     </p>
                 </GridItem>
                 <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <h4><b>7. Rights of the data subject</b></h4>
+                    <h4><b>8. Rights of the data subject</b></h4>
                     <br />
                     <p>
                         According to the data subject rights of the GDPR the following actions can be carried out or referred to by the data subject.
@@ -98,10 +100,6 @@ const GDPR = () => {
                         <li>Automated individual decision-making, including profiling (Art. 22 GDPR)</li>
                         <li>Restrictions (Art. 23 GDPR)</li>
                     </ul>
-                </GridItem>
-                <GridItem rowSpan={1} colSpan={1} p='5' boxShadow='md' rounded='md' bg='gray.200'>
-                    <h4><b>8. Legal basis for the processing </b></h4>
-                    <p>Art. 6(1) lit. a GDPR serves as the legal basis for processing operations for which we obtain consent for a specific processing purpose. The same applies to such processing operations which are necessary for carrying out pre-contractual measures, for example in the case of inquiries concerning our products or services. Is our company subject to a legal obligation by which processing of personal data is required, such as for the fulfillment of tax obligations, the processing is based on Art. 6(1) lit. c GDPR.Finally, processing operations could be based on Article 6(1) lit. f GDPR. This legal basis is used for processing operations which are not covered by any of the abovementioned legal grounds, if processing is necessary for the purposes of the legitimate interests pursued by our company or by a third party, except where such interests are overridden by the interests or fundamental rights and freedoms of the data subject which require protection of personal data. Such processing operations are particularly permissible because they have been specifically mentioned by the European legislator. He considered that a legitimate interest could be assumed if the data subject is a client of the controller (Recital 47 Sentence 2 GDPR).</p>
                 </GridItem>
             </Grid>
 
