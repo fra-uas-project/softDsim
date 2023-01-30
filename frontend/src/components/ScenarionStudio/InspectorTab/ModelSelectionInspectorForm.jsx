a5718ccea78727af704243aaab3d05a282626af7
Leonard Husske <leonard.husske@t-online.de>
2023-01-30 22:39:49 +0100

    Refactor ModelSelectionInspectorForm to use only props and not state anymore
    * Needed so that the values in the form get rendered when the value changed from outside the component

--- frontend/src/components/ScenarionStudio/InspectorTab/ModelSelectionInspectorForm.jsx
+++ frontend/src/components/ScenarionStudio/InspectorTab/ModelSelectionInspectorForm.jsx
@@ -5,53 +5,51 @@ import {
     Editable,
     EditableInput,
     EditablePreview,
-    FormControl, FormErrorMessage, FormHelperText,
+    FormControl,
+    FormErrorMessage,
+    FormHelperText,
     FormLabel,
     VStack
 } from "@chakra-ui/react";
 import MarkdownTextfield from "./MarkdownTextfield";
-import {useEffect, useState} from "react";
 import DeleteButton from "./DeleteButton";
 import {getErrorColor, getErrorMessage, isError} from "../../../utils/utils";
 
 const ModelSelectionInspectorForm = (props) => {
 
-    const [displayName, setDisplayName] = useState(props.modelSelectionData?.displayName);
-    const [models, setModels] = useState(props.modelSelectionData?.models);
-
     const allModels = ["Kanban", "Scrum", "Waterfall"];
 
     const onChangeModels = (event) => {
-        if(models.includes(event.target.value)) {
-            setModels(models.filter((element) => {return element !== event.target.value}))
-        }else {
-            setModels(prevState => [...prevState, event.target.value])
+        if (props.modelSelectionData.models.includes(event.target.value)) {
+            props.updateEditorList(
+                (draft) => {
+                    const component = draft.find((component) => component.id === props.modelSelectionData.id)
+                    component.models = component.models.filter((element) => {
+                        return element !== event.target.value
+                    });
+                }
+            )
+        } else {
+            props.updateEditorList(
+                (draft) => {
+                    const component = draft.find((component) => component.id === props.modelSelectionData.id)
+                    component.models = [...component.models, event.target.value]
+                }
+            )
         }
     }
 
     const onChangeDisplayName = (value) => {
-        setDisplayName(value)
-    }
-
-    const onSubmitDisplayName = () => {
-        props.updateEditorList(
-           (draft) => {
-            const component = draft.find((component) => component.id === props.modelSelectionData.id)
-            component.displayName = displayName;
-        })
-    }
-
-    useEffect(() => {
         props.updateEditorList(
             (draft) => {
                 const component = draft.find((component) => component.id === props.modelSelectionData.id)
-                component.models = models;
+                component.displayName = value;
             })
-    }, [models, props])
+    }
 
     return (
         <VStack maxW="300px" alignItems="flex-start">
-            <Editable value={displayName} w="full" fontWeight="bold" onChange={(value) => onChangeDisplayName(value)} onSubmit={onSubmitDisplayName}>
+            <Editable value={props.modelSelectionData.displayName} w="full" fontWeight="bold" onChange={(value) => onChangeDisplayName(value)}>
                 <EditablePreview
                     w="full"
                     _hover={{
@@ -81,7 +79,13 @@ const ModelSelectionInspectorForm = (props) => {
             <FormControl flexDir="column" display="flex" isInvalid={isError(props.validationErrors, props.modelSelectionData.id, "models")}>
                 <FormLabel color="gray.400" htmlFor="">Available Management Models</FormLabel>
                 {allModels.map((value, index) => {
-                    return <Checkbox key={index} spacing='1rem' mb="0.5rem" value={value} onChange={(event) => onChangeModels(event)} isChecked={models.includes(value)}>{value}</Checkbox> // todo add error border color if needed
+                    return <Checkbox key={index}
+                                     spacing='1rem'
+                                     mb="0.5rem"
+                                     value={value}
+                                     onChange={(event) => onChangeModels(event)}
+                                     isChecked={props.modelSelectionData.models.includes(value)}>{value}
+                    </Checkbox> // todo add error border color if needed
                 })}
                 {isError(props.validationErrors, props.modelSelectionData.id, "models") ?
                     <FormErrorMessage color={getErrorColor(props.validationErrors, props.modelSelectionData.id, "models")}>
