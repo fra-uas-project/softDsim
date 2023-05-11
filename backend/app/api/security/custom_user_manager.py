acd8cbc776e155f1f4cb010d9b840e52f12f2430
Nizar-Kaouri <p.kaouri@gmail.com>
2023-05-11 11:41:34 +0200

    SuperUser Bug Fix + Duplicate Button

--- backend/app/api/security/custom_user_manager.py
+++ backend/app/api/security/custom_user_manager.py
@@ -1,10 +1,50 @@
 # src/users/model.py
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.hashers import make_password
+from django.apps import apps
+
+
+# from django.utils.translation import ugettext_lazy as _
 
 # WE MIGHT NEED THIS IF WE WANT TO USE EMAIL AS USERNAME
 
-# class CustomUserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("staff", True)
+        extra_fields.setdefault("creator", True)
+        extra_fields.setdefault("admin", True)
+
+        print("CREATED")
+        return self._create_user(username, email, password, **extra_fields)
+
+
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
 #     """
 #     Custom user model manager where email is the unique identifiers
 #     for authentication instead of usernames.
@@ -35,3 +75,6 @@ from django.utils.translation import ugettext_lazy as _
 #         if extra_fields.get("is_superuser") is not True:
 #             raise ValueError(_("Superuser must have is_superuser=True."))
 #         return self.create_user(email, password, **extra_fields)
+
+
+
