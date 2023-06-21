f9936e7f4caaa8cab8e85637d313d653bb3a099d
Zersai <83419263+Zersai@users.noreply.github.com>
2023-06-21 21:22:22 +0200

    Neue Css Styles hinzugefügt

--- frontend/src/components/Simulation/Event/Event.jsx
+++ frontend/src/components/Simulation/Event/Event.jsx
@@ -2,13 +2,19 @@ import { Flex } from "@chakra-ui/react";
 import MarkdownDisplay from "../../MarkdownDisplay";
 
 const Event = (props) => {
+    const divStyle = {
+        border: '1px',
+        padding: '10px',
+        marginBottom: '20px',
+        width: '585px',
+        overflow: 'auto'
+    };
+
     return (
-        <Flex>
-		<div style={{ width: '585px' }}>
-            <MarkdownDisplay markdownText={props.eventText}/>
-		</div>
-        </Flex>
-    )
+        <div style={divStyle}>
+            <MarkdownDisplay markdownText={props.eventText} />
+        </div>
+    );
 }
 
 export default Event
