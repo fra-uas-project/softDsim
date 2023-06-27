c2d442d5609bc4624b3ea6d7da77400f643595aa
Zersai <83419263+Zersai@users.noreply.github.com>
2023-06-27 19:39:53 +0200

    Nun ist es auch responsive

--- frontend/src/components/Simulation/Event/Event.jsx
+++ frontend/src/components/Simulation/Event/Event.jsx
@@ -2,19 +2,21 @@ import { Flex } from "@chakra-ui/react";
 import MarkdownDisplay from "../../MarkdownDisplay";
 
 const Event = (props) => {
-    const divStyle = {
-        border: '1px',
-        padding: '10px',
-        marginBottom: '20px',
-        width: '585px',
-        overflow: 'auto'
-    };
+  const divStyle = {
+    border: '1px',
+    padding: '10px',
+    marginBottom: '20px',
+    width: '100%', // Adjusted width to take full width of the container
+    overflow: 'auto'
+  };
 
-    return (
-        <div style={divStyle}>
-            <MarkdownDisplay markdownText={props.eventText} />
-        </div>
-    );
+  return (
+    <Flex justify="center">
+      <div style={divStyle}>
+        <MarkdownDisplay markdownText={props.eventText} />
+      </div>
+    </Flex>
+  );
 }
 
-export default Event
+export default Event;
