962bfd0e7b94fc0cdc710ffa7383858bef779fbf
Amine Amzil <>
2023-06-30 16:22:13 +0200

    create decorator to check scenario accessability

--- backend/app/decorators/decorators.py
+++ backend/app/decorators/decorators.py
@@ -1,22 +1,30 @@
+from app.models.course import Course
+from app.models.template_scenario import TemplateScenario
+from custom_user.models import User
 from rest_framework import status
 from rest_framework.response import Response
 
 
 def allowed_roles(allowed_roles=[]):
     def decorator(view_class):
-        def wrapper_func(self, request, *args, **kwargs):  # Update the parameter list to include 'self'
+        # Update the parameter list to include 'self'
+        def wrapper_func(self, request, *args, **kwargs):
+            print("allowed_roles")
             if "all" in allowed_roles:
-                return view_class(self, request, *args, **kwargs)  # Pass 'self' as the first argument
+                # Pass 'self' as the first argument
+                return view_class(self, request, *args, **kwargs)
 
             user = request.user
 
             # admin user can call any function
             if user.admin:
-                return view_class(self, request, *args, **kwargs)  # Pass 'self' as the first argument
+                # Pass 'self' as the first argument
+                return view_class(self, request, *args, **kwargs)
 
             for role in allowed_roles:
                 if getattr(user, role, False):
-                    return view_class(self, request, *args, **kwargs)  # Pass 'self' as the first argument
+                    # Pass 'self' as the first argument
+                    return view_class(self, request, *args, **kwargs)
 
             return Response(
                 {
@@ -27,4 +35,54 @@ def allowed_roles(allowed_roles=[]):
 
         return wrapper_func
 
+    return decorator
+
+
+def has_access_to_scenario(field_name: str = None, template_id_in_request_data: bool = True):
+    """
+    Checks if the current user is alloed to access a scenario
+    """
+    def decorator(view_class):
+        def wrapper_func(self, request, *args, **kwargs):
+            print("has_access_to_scenario")
+            user: User = request.user
+
+            if user is None:
+                return Response(
+                    {
+                        "message": f"User is not authenticated. Please login first with username and password."
+                    },
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+
+            has_role: bool = user.admin or user.staff or user.creator
+
+            if has_role:
+                return view_class(self, request, *args, **kwargs)
+
+            template_id: str = None
+
+            if template_id_in_request_data:
+                template_id = request.data.get(field_name.strip(), None)
+            elif field_name.strip() in kwargs:
+                template_id = kwargs[field_name.strip()]
+
+            if template_id is None:
+                return view_class(self, request, *args, **kwargs)
+
+            is_allowed: bool = Course.objects.filter(
+                users__id=user.id, scenarios__id=template_id
+            ).exists()
+
+            if is_allowed:
+                return view_class(self, request, *args, **kwargs)
+            else:
+                return Response(
+                    {
+                        "message": f"User {user.username} is not authorized to access scenario {template_id}."
+                    },
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+
+        return wrapper_func
     return decorator
